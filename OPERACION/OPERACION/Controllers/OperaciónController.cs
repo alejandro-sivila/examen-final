200b747b566fe44c98050f17478a49b9fea46fbe
Alejandro Horacio Sivila Guzman <a2019116316@estudiantes.upsa.edu.bo>
2020-12-12 11:21:27 -0400

    1ra pregunta terminada

---
+++ OPERACION/OPERACION/Controllers/OperaciónController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OPERACION.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperaciónController : ControllerBase
+    {
+        [HttpGet]
+        public string cuñape(int numero)
+        {
+            string respuesta = "";
+            if (numero < 0)
+            {
+                respuesta = "ERROR";
+            }
+            if (numero == 0)
+            {
+                respuesta = "Realizado por Alejandro Horacio Sivila Guzman";
+            }
+            if (numero > 0)
+            {
+                respuesta = "https://image.freepik.com/vector-gratis/numeros-cartel-imagen_1639-6453.jpg";
+            }
+            return (respuesta);
+        }
+    }
+
+}
